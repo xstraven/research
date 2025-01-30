dc0babb1c5ee97f3c5d135edea07f2689cb7c06c
David Hinrichs <dvd.hnrchs@gmail.com>
2025-01-30 15:56:23 +0100

    add langfuse instructor test for vertex

--- src/research/llms/test_langfuse.py
+++ src/research/llms/test_langfuse.py
@@ -1,38 +1,67 @@
-import base64
 import vertexai
-from vertexai.generative_models import GenerativeModel, Part, FinishReason
-import vertexai.preview.generative_models as generative_models
-
+from vertexai.generative_models import GenerativeModel
+from pydantic import BaseModel
 from langfuse.decorators import langfuse_context, observe
-import os
+import instructor
 
-GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+
+vertexai.init(location="us-central1")
+
+
+class User(BaseModel):
+    name: str
+    age: int
 
 
 @observe(as_type="generation")
-def vertex_generate_content(input, model_name="gemini-pro"):
-    vertexai.init(project=GCP_PROJECT_ID, location="us-central1")
-    model = GenerativeModel(model_name)
-    response = model.generate_content(
-        [input],
-        generation_config={
-            "max_output_tokens": 8192,
-            "temperature": 1,
-            "top_p": 0.95,
-        },
+def instructor_parse_w_vertex(
+    content,
+    response_model,
+    model_name,
+    **model_params,
+):
+    client = instructor.from_vertexai(
+        client=GenerativeModel(model_name),
+        mode=instructor.Mode.VERTEXAI_TOOLS,
+    )
+    response = client.create(
+        messages=[
+            {
+                "role": "user",
+                "content": content,
+            }
+        ],
+        response_model=response_model,
+        **model_params,
     )
 
     # pass model, model input, and usage metrics to Langfuse
     langfuse_context.update_current_observation(
-        input=input,
+        input=content,
         model=model_name,
-        usage_details={
-            "input": response.usage_metadata.prompt_token_count,
-            "output": response.usage_metadata.candidates_token_count,
-            "total": response.usage_metadata.total_token_count,
+        usage_details={  # instructor puts the original response under ._raw_response
+            "input": response._raw_response.usage_metadata.prompt_token_count,
+            "output": response._raw_response.usage_metadata.candidates_token_count,
+            "total": response._raw_response.usage_metadata.total_token_count,
+        },
+        metadata={
+            "model": model_name,
+            "gen_config": model_params,
+            "response_schema": response_model.model_json_schema(),
         },
     )
-    return response.candidates[0].content.parts[0].text
+    return response
 
 
-vertex_generate_content("The quick brown fox jumps over the lazy dog")
+@observe()
+def instructor_parse(content, response_model, model_name, **model_params):
+    # do some more setup
+    return instructor_parse_w_vertex(
+        content, response_model, model_name, **model_params
+    )
+
+
+resp = instructor_parse(
+    "Jason is 25 years old.", User, "gemini-1.5-pro-preview-0409"
+)
+resp
