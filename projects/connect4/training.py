1c8b06af4e7676271ad07a4004f3b86eb407c44a
David Hinrichs <dvd.hnrchs@gmail.com>
2026-01-25 09:46:45 +0100

    dataloader bugfixes

--- projects/connect4/training.py
+++ projects/connect4/training.py
@@ -24,22 +24,34 @@ class DataLoader:
     def build_features(self):
         for game in self.experiment.games:
             boards = [get_empty_board(), get_empty_board()]
+            player1 = game.moves[0][0]
+            player2 = game.moves[1][0]
             dc_return = self.reward * (self.gamma ** (len(game.moves) - 1))
+            if game.winner == 0:
+                dc_return = 0
+            if game.winner == player2:
+                dc_return = -dc_return
             for move in game.moves:
                 player = move[0]
                 x, y = move[1], move[2]
-
+                player_return = dc_return if player == player1 else -dc_return
+                player_boards = (
+                    deepcopy(boards)
+                    if player == player1
+                    else deepcopy(boards[::-1])
+                )
+                player_boards[1] = [
+                    [-cell for cell in row] for row in player_boards[1]
+                ]
                 t = Transition(
-                    boards=deepcopy(boards),
+                    boards=player_boards,
                     player=player,
                     action=y,
-                    dc_return=dc_return,
+                    dc_return=player_return,
                 )
                 self.samples.append(t)
-                boards[0 if player == 1 else 1][x][y] = 1 if player == 1 else -1
-                dc_return = (1 if player == 1 else -1) * (
-                    dc_return / self.gamma
-                )
+                boards[0 if player == player1 else 1][x][y] = 1
+                dc_return = dc_return / self.gamma
 
                 # t = Transition(board=, p, a, dc_return)
 
